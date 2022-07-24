0a42a13a17b67b1f3f2ea6cae9a3fcc0387b37f3
maciej.morawski <62252131+mpfmorawski@users.noreply.github.com>
2022-07-24 23:32:55 +0200

    Script for going to starting line before yellow gate task in simulation 2.3dev version

---
+++ go-to-starting-line.py
@@ -0,0 +1,50 @@
+import time
+from okonClient import OkonClient, PacketType, PacketFlag
+
+def handle_simulation_reset(args = None) -> None:
+    print('sim resetted')
+
+def handle_ping(args = None) -> None:
+    global sum, n
+    cur_ping = round((time.time() - float(args))*10000)/10
+    sum += cur_ping
+    n += 1
+    print(f'avg RTT is {sum/n} ping:{cur_ping}')
+    time.sleep(1)
+
+oc = OkonClient(ip="127.0.0.1", port=44210, sync_interval=.05, debug=False) #ping simRST hitNGZ hitFZ packet error disconnect # run on separate thread
+oc.on_event('simRST', handle_simulation_reset) 
+oc.on_event('ping', handle_ping)
+okon = oc.okon
+sim = oc.simulation
+
+sum = 0 # ping checking
+n = 0
+if oc.connect():
+    sim.reset()
+    time.sleep(.5)
+
+    print("Initialization.")
+    
+    okon.disarm_motors()
+    okon.set_stable_vel(x=0,y=0,z=0)
+    okon.set_depth(.6)
+    okon.set_stable_rot(y=okon.sens['imu']['rot']['y'])
+
+    okon.arm_motors()
+    time.sleep(1)
+
+    print("Reaching the starting line...")
+
+    okon.set_stable_rot(y=0, add=True)
+    while not okon.reachedTargetRotation(1):
+        time.sleep(.1)
+
+    okon.set_stable_vel(z=-1)
+    time.sleep(.5)
+    okon.set_stable_vel(z=0)
+    time.sleep(1.5)
+    print("Starting line reached.")
+
+else:
+    print("Error")
