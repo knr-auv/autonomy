3f8e84bb7adabc931e704076bcff4656ec7a970f
maciej.morawski <62252131+mpfmorawski@users.noreply.github.com>
2022-08-03 01:29:25 +0200

    Reformatting files using organization settings.json file

--- okon_actions.py
+++ okon_actions.py
@@ -11,12 +11,7 @@ from okon_client import Okon
 
 
 class SetDepth(py_trees.behaviour.Behaviour):
-
-    def __init__(self,
-                 name: str = "set depth",
-                 okon: Okon = None,
-                 depth: float = 0.6,
-                 delta: float = .05):
+    def __init__(self, name: str = "set depth", okon: Okon = None, depth: float = 0.6, delta: float = 0.05):
         super().__init__(name)
         self.okon = okon
         self.depth = depth
@@ -28,20 +23,19 @@ class SetDepth(py_trees.behaviour.Behaviour):
 
     def update(self) -> py_trees.common.Status:
         self.okon.set_depth(self.depth)
-        new_status = py_trees.common.Status.SUCCESS if self.okon.reachedTargetDepth(
-            self.delta) else py_trees.common.Status.RUNNING
+        new_status = (
+            py_trees.common.Status.SUCCESS
+            if self.okon.reachedTargetDepth(self.delta)
+            else py_trees.common.Status.RUNNING
+        )
         if new_status == py_trees.common.Status.SUCCESS:
-            self.feedback_message = "Target depth of {0} m reached.".format(
-                self.depth)
+            self.feedback_message = "Target depth of {0} m reached.".format(self.depth)
         else:
             self.feedback_message = "Current depth {0:.3f}. Waiting for target depth of {1} m.".format(
-                self.okon.sens["baro"] / 1000 / 9.81, self.depth)
-        self.logger.debug(
-            "%s.update()[%s->%s][%s]" % (
-                self.__class__.__name__,
-                self.status, new_status,
-                self.feedback_message
+                self.okon.sens["baro"] / 1000 / 9.81, self.depth
             )
+        self.logger.debug(
+            "%s.update()[%s->%s][%s]" % (self.__class__.__name__, self.status, new_status, self.feedback_message)
         )
         return new_status
 
@@ -50,18 +44,11 @@ class SetDepth(py_trees.behaviour.Behaviour):
 
     def terminate(self, new_status):
         """Nothing to clean up in this example."""
-        self.logger.debug(
-            "%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
+        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
 
 
 class SetVelocity(py_trees.behaviour.Behaviour):
-
-    def __init__(self,
-                 name: str = "set velocity",
-                 okon: Okon = None,
-                 x: float = 0.,
-                 y: float = 0.,
-                 z: float = 0.):
+    def __init__(self, name: str = "set velocity", okon: Okon = None, x: float = 0.0, y: float = 0.0, z: float = 0.0):
         super().__init__(name)
         self.okon = okon
         self.x = x
@@ -77,58 +64,50 @@ class SetVelocity(py_trees.behaviour.Behaviour):
         new_status = py_trees.common.Status.SUCCESS
         if new_status == py_trees.common.Status.SUCCESS:
             self.feedback_message = "Speed set as: Vx = {0:.3f} Vy = {1:.3f} Vz = {2:.3f}.".format(
-                self.x, self.y, self.z)
-        self.logger.debug(
-            "%s.update()[%s->%s][%s]" % (
-                self.__class__.__name__,
-                self.status, new_status,
-                self.feedback_message
+                self.x, self.y, self.z
             )
+        self.logger.debug(
+            "%s.update()[%s->%s][%s]" % (self.__class__.__name__, self.status, new_status, self.feedback_message)
         )
         return new_status
 
     def terminate(self, new_status):
         """Nothing to clean up in this example."""
-        self.logger.debug(
-            "%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
+        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
 
 
 class Rotate(py_trees.behaviour.Behaviour):
-
-    def __init__(self,
-                 name: str = "rotate",
-                 okon: Okon = None,
-                 add_angle: float = 45.,
-                 delta: float = 1.):
+    def __init__(self, name: str = "rotate", okon: Okon = None, add_angle: float = 45.0, delta: float = 1.0):
         super().__init__(name)
         self.okon = okon
         self.add_angle = add_angle
-        self.target_angle = self.okon.sens['imu']['rot']['y'] + self.add_angle
+        self.target_angle = self.okon.sens["imu"]["rot"]["y"] + self.add_angle
         self.delta = delta
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def initialise(self):
-        self.target_angle = self.okon.sens['imu']['rot']['y'] + self.add_angle
-        if self.target_angle < 0.:
-            self.target_angle += 360.
+        self.target_angle = self.okon.sens["imu"]["rot"]["y"] + self.add_angle
+        if self.target_angle < 0.0:
+            self.target_angle += 360.0
         self.logger.debug("%s.initialise()" % (self.__class__.__name__))
 
     def update(self):
         self.okon.set_stable_rot(y=self.target_angle)
-        new_status = py_trees.common.Status.SUCCESS if self.okon.reachedTargetRotation(
-            self.delta) else py_trees.common.Status.RUNNING
+        new_status = (
+            py_trees.common.Status.SUCCESS
+            if self.okon.reachedTargetRotation(self.delta)
+            else py_trees.common.Status.RUNNING
+        )
         if new_status == py_trees.common.Status.SUCCESS:
-            self.feedback_message = "Target rotation of {0} degrees reached.".format(
-                self.target_angle)
+            self.feedback_message = "Target rotation of {0} degrees reached.".format(self.target_angle)
         else:
-            self.feedback_message = "Current rotation is {0:.3f} degrees. Waiting for target rotation of {1:.3f} degrees.".format(
-                self.okon.sens['imu']['rot']['y'], self.target_angle)
-        self.logger.debug(
-            "%s.update()[%s->%s][%s]" % (
-                self.__class__.__name__,
-                self.status, new_status,
-                self.feedback_message
+            self.feedback_message = (
+                "Current rotation is {0:.3f} degrees. Waiting for target rotation of {1:.3f} degrees.".format(
+                    self.okon.sens["imu"]["rot"]["y"], self.target_angle
+                )
             )
+        self.logger.debug(
+            "%s.update()[%s->%s][%s]" % (self.__class__.__name__, self.status, new_status, self.feedback_message)
         )
         return new_status
 
@@ -137,72 +116,59 @@ class Rotate(py_trees.behaviour.Behaviour):
 
     def terminate(self, new_status):
         """Nothing to clean up in this example."""
-        self.logger.debug(
-            "%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
+        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
 
 
 class RotateDeltaYawAngle(py_trees.behaviour.Behaviour):
-
-    def __init__(self,
-                 name: str = "rotate delta yaw angle",
-                 okon: Okon = None,
-                 delta: float = 1.):
+    def __init__(self, name: str = "rotate delta yaw angle", okon: Okon = None, delta: float = 1.0):
         super().__init__(name)
         self.okon = okon
         self.delta = delta
         self.blackboard = self.attach_blackboard_client()
-        self.blackboard.register_key(
-            key="deltaYaw", access=py_trees.common.Access.READ)
-        self.target_angle = self.okon.sens['imu']['rot']['y']
+        self.blackboard.register_key(key="deltaYaw", access=py_trees.common.Access.READ)
+        self.target_angle = self.okon.sens["imu"]["rot"]["y"]
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def initialise(self):
-        self.target_angle = self.okon.sens['imu']['rot']['y'] + \
-            self.blackboard.deltaYaw
-        if self.target_angle < 0.:
-            self.target_angle += 360.
+        self.target_angle = self.okon.sens["imu"]["rot"]["y"] + self.blackboard.deltaYaw
+        if self.target_angle < 0.0:
+            self.target_angle += 360.0
         self.logger.debug("%s.initialise()" % (self.__class__.__name__))
 
     def update(self):
         self.okon.set_stable_rot(y=self.target_angle)
-        new_status = py_trees.common.Status.SUCCESS if self.okon.reachedTargetRotation(
-            self.delta) else py_trees.common.Status.RUNNING
+        new_status = (
+            py_trees.common.Status.SUCCESS
+            if self.okon.reachedTargetRotation(self.delta)
+            else py_trees.common.Status.RUNNING
+        )
         if new_status == py_trees.common.Status.SUCCESS:
-            self.feedback_message = "Target rotation of {0} degrees reached.".format(
-                self.target_angle)
+            self.feedback_message = "Target rotation of {0} degrees reached.".format(self.target_angle)
         else:
-            self.feedback_message = "Current rotation is {0:.3f} degrees. Waiting for target rotation of {1:.3f} degrees.".format(
-                self.okon.sens['imu']['rot']['y'], self.target_angle)
-        self.logger.debug(
-            "%s.update()[%s->%s][%s]" % (
-                self.__class__.__name__,
-                self.status, new_status,
-                self.feedback_message
+            self.feedback_message = (
+                "Current rotation is {0:.3f} degrees. Waiting for target rotation of {1:.3f} degrees.".format(
+                    self.okon.sens["imu"]["rot"]["y"], self.target_angle
+                )
             )
+        self.logger.debug(
+            "%s.update()[%s->%s][%s]" % (self.__class__.__name__, self.status, new_status, self.feedback_message)
         )
         return new_status
 
     def terminate(self, new_status):
         """Nothing to clean up in this example."""
-        self.logger.debug(
-            "%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
+        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
 
 
 class TryDetectNTimes(py_trees.behaviour.Behaviour):
-
-    def __init__(self,
-                 name: str = "try detect n times",
-                 okon: Okon = None,
-                 object: str = "gate",
-                 n: int = 3):
+    def __init__(self, name: str = "try detect n times", okon: Okon = None, object: str = "gate", n: int = 3):
         super().__init__(name)
         self.okon = okon
         self.object = object
         self.n = n
         self.counter = 1
         self.blackboard = self.attach_blackboard_client()
-        self.blackboard.register_key(
-            key="detection", access=py_trees.common.Access.WRITE)
+        self.blackboard.register_key(key="detection", access=py_trees.common.Access.WRITE)
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def initialise(self):
@@ -220,41 +186,29 @@ class TryDetectNTimes(py_trees.behaviour.Behaviour):
             new_status = py_trees.common.Status.RUNNING
 
         if new_status == py_trees.common.Status.SUCCESS:
-            self.feedback_message = "Object {0} detected in attempt number {1}".format(
-                self.object, self.counter)
+            self.feedback_message = "Object {0} detected in attempt number {1}".format(self.object, self.counter)
         else:
-            self.feedback_message = "Object {0} undetected in attempt number {1}".format(
-                self.object, self.counter)
+            self.feedback_message = "Object {0} undetected in attempt number {1}".format(self.object, self.counter)
 
         self.counter += 1
 
         self.logger.debug(
-            "%s.update()[%s->%s][%s]" % (
-                self.__class__.__name__,
-                self.status, new_status,
-                self.feedback_message
-            )
+            "%s.update()[%s->%s][%s]" % (self.__class__.__name__, self.status, new_status, self.feedback_message)
         )
         return new_status
 
     def terminate(self, new_status):
         """Nothing to clean up in this example."""
-        self.logger.debug(
-            "%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
+        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
 
 
 class CalculateDeltaYaw(py_trees.behaviour.Behaviour):
-
-    def __init__(self,
-                 name: str = "calculate delta yaw",
-                 okon: Okon = None):
+    def __init__(self, name: str = "calculate delta yaw", okon: Okon = None):
         super().__init__(name)
         self.okon = okon
         self.blackboard = self.attach_blackboard_client()
-        self.blackboard.register_key(
-            key="detection", access=py_trees.common.Access.READ)
-        self.blackboard.register_key(
-            key="deltaYaw", access=py_trees.common.Access.WRITE)
+        self.blackboard.register_key(key="detection", access=py_trees.common.Access.READ)
+        self.blackboard.register_key(key="deltaYaw", access=py_trees.common.Access.WRITE)
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def initialise(self):
@@ -266,49 +220,38 @@ class CalculateDeltaYaw(py_trees.behaviour.Behaviour):
             new_status = py_trees.common.Status.SUCCESS
             hfov = 60
             gate = detection[0]
-            if gate['distance'] < 1:
+            if gate["distance"] < 1:
                 self.deltaYaw = 0
             else:
-                center = (gate['max']['x'] + gate['min']['x'])/2 * 2 - 1
-                cameraPlaneX = 1./math.tan(hfov/2/180*math.pi)
-                self.deltaYaw = math.atan(center/cameraPlaneX)/math.pi*180
+                center = (gate["max"]["x"] + gate["min"]["x"]) / 2 * 2 - 1
+                cameraPlaneX = 1.0 / math.tan(hfov / 2 / 180 * math.pi)
+                self.deltaYaw = math.atan(center / cameraPlaneX) / math.pi * 180
             self.blackboard.deltaYaw = self.deltaYaw
         else:
             new_status = py_trees.common.Status.FAILURE
 
         if new_status == py_trees.common.Status.SUCCESS:
-            self.feedback_message = "Delta Yaw was calculated and is equal to {0}".format(
-                self.deltaYaw)
+            self.feedback_message = "Delta Yaw was calculated and is equal to {0}".format(self.deltaYaw)
         else:
             self.feedback_message = "There were no objects in the detection parameter in blackboard."
 
         self.logger.debug(
-            "%s.update()[%s->%s][%s]" % (
-                self.__class__.__name__,
-                self.status, new_status,
-                self.feedback_message
-            )
+            "%s.update()[%s->%s][%s]" % (self.__class__.__name__, self.status, new_status, self.feedback_message)
         )
         return new_status
 
     def terminate(self, new_status):
         """Nothing to clean up in this example."""
-        self.logger.debug(
-            "%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
+        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
 
 
 class IsGateFarEnough(py_trees.behaviour.Behaviour):
-
-    def __init__(self,
-                 name: str = "calculate delta yaw",
-                 okon: Okon = None,
-                 max_distance: float = 1.5):
+    def __init__(self, name: str = "calculate delta yaw", okon: Okon = None, max_distance: float = 1.5):
         super().__init__(name)
         self.okon = okon
         self.max_distance = max_distance
         self.blackboard = self.attach_blackboard_client()
-        self.blackboard.register_key(
-            key="detection", access=py_trees.common.Access.READ)
+        self.blackboard.register_key(key="detection", access=py_trees.common.Access.READ)
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def initialise(self):
@@ -316,36 +259,27 @@ class IsGateFarEnough(py_trees.behaviour.Behaviour):
 
     def update(self):
         gate = self.blackboard.detection[0]
-        new_status = py_trees.common.Status.SUCCESS if gate['distance'] > self.max_distance else py_trees.common.Status.FAILURE
+        new_status = (
+            py_trees.common.Status.SUCCESS if gate["distance"] > self.max_distance else py_trees.common.Status.FAILURE
+        )
 
         if new_status == py_trees.common.Status.SUCCESS:
-            self.feedback_message = "Gate is in distance of {0:.3f} m".format(
-                gate['distance'])
+            self.feedback_message = "Gate is in distance of {0:.3f} m".format(gate["distance"])
         else:
-            self.feedback_message = "Gate is closer than max distance set to {0:.3f}m.".format(
-                self.max_distance)
+            self.feedback_message = "Gate is closer than max distance set to {0:.3f}m.".format(self.max_distance)
 
         self.logger.debug(
-            "%s.update()[%s->%s][%s]" % (
-                self.__class__.__name__,
-                self.status, new_status,
-                self.feedback_message
-            )
+            "%s.update()[%s->%s][%s]" % (self.__class__.__name__, self.status, new_status, self.feedback_message)
         )
         return new_status
 
     def terminate(self, new_status):
         """Nothing to clean up in this example."""
-        self.logger.debug(
-            "%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
+        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
 
 
 class Wait(py_trees.behaviour.Behaviour):
-
-    def __init__(self,
-                 name: str = "wait",
-                 okon: Okon = None,
-                 secs: float = 0.):
+    def __init__(self, name: str = "wait", okon: Okon = None, secs: float = 0.0):
         super().__init__(name)
         self.okon = okon
         self.secs = secs
@@ -358,27 +292,19 @@ class Wait(py_trees.behaviour.Behaviour):
         time.sleep(self.secs)
         new_status = py_trees.common.Status.SUCCESS
         if new_status == py_trees.common.Status.SUCCESS:
-            self.feedback_message = "Robot waited for {0} secunds.".format(
-                self.secs)
+            self.feedback_message = "Robot waited for {0} secunds.".format(self.secs)
         self.logger.debug(
-            "%s.update()[%s->%s][%s]" % (
-                self.__class__.__name__,
-                self.status, new_status,
-                self.feedback_message
-            )
+            "%s.update()[%s->%s][%s]" % (self.__class__.__name__, self.status, new_status, self.feedback_message)
         )
         return new_status
 
     def terminate(self, new_status):
         """Nothing to clean up in this example."""
-        self.logger.debug(
-            "%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
+        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
 
 
 class Exit(py_trees.behaviour.Behaviour):
-
-    def __init__(self,
-                 name: str = "exit"):
+    def __init__(self, name: str = "exit"):
         super().__init__(name)
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
--- okon_actions_example.py
+++ okon_actions_example.py
@@ -12,31 +12,32 @@ import py_trees
 from okon_actions import Rotate, SetDepth
 from okon_client import OkonClient
 
-oc = OkonClient(ip="127.0.0.1", port=44210, sync_interval=.05, debug=False)
+oc = OkonClient(ip="127.0.0.1", port=44210, sync_interval=0.05, debug=False)
 oc.connect()
-time.sleep(1.)
+time.sleep(1.0)
+
 
 def create_root():
-    """ description
+    """description
 
     Long description
     """
     root = py_trees.composites.Sequence("Sequence")
-    set_depth_action_1 = SetDepth(name = "Set Depth to 0.2 m",okon=oc.okon, depth=0.2, delta=0.005)
-    rotate_action_2 = Rotate(name = "Turn left",okon=oc.okon, add_angle=-45., delta=1.)
-    set_depth_action_3 = SetDepth(name = "Set Depth to 0.8 m",okon=oc.okon, depth=0.8, delta=0.005)
-    rotate_action_4 = Rotate(name = "Turn right",okon=oc.okon, add_angle=45., delta=1.)
+    set_depth_action_1 = SetDepth(name="Set Depth to 0.2 m", okon=oc.okon, depth=0.2, delta=0.005)
+    rotate_action_2 = Rotate(name="Turn left", okon=oc.okon, add_angle=-45.0, delta=1.0)
+    set_depth_action_3 = SetDepth(name="Set Depth to 0.8 m", okon=oc.okon, depth=0.8, delta=0.005)
+    rotate_action_4 = Rotate(name="Turn right", okon=oc.okon, add_angle=45.0, delta=1.0)
     root.add_children([set_depth_action_1, rotate_action_2, set_depth_action_3, rotate_action_4])
     return root
 
 
 def main():
-    """ Main function of the script"""
+    """Main function of the script"""
     py_trees.logging.level = py_trees.logging.Level.DEBUG
 
     root = create_root()
     root.setup_with_descendants()
-    
+
     for i in range(0, 30):
         try:
             tick_string = "Tick"
@@ -44,10 +45,11 @@ def main():
             root.tick_once()
             print("\n")
             print(py_trees.display.unicode_tree(root=root, show_status=True))
-            time.sleep(.5)
+            time.sleep(0.5)
         except KeyboardInterrupt:
             break
     print("\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
